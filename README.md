afdb05ae65f5663f8b6d32b3a7f60bf32a0ca800
Naram99 <131804895+Naram99@users.noreply.github.com>
2024-02-29 20:24:55 +0100

    Readme commit
    Index.ts modified, readme.md added

---
+++ README.md
@@ -0,0 +1,20 @@
+# Festival backend
+
+This is a typescript project for an Event manager to manage visitors and events.
+
+## Installation
+
+You have to have NodeJS installed on your computer.
+
+## Usage
+
+Open the project's folder in a terminal, and type in npm start.
+
+```bash
+npm start
+```
+
+## GitHub page
+
+[GitHub](https://github.com/Naram99/TypeScriptVizsga)
+
--- src/index.ts
+++ src/index.ts
@@ -25,21 +25,39 @@ const art = new Event(
     new Map<number, Visitor>
     );
 
+const art2 = new Event(
+    3, 
+    "MűvészVáros", 
+    "Szeged", 
+    new Date("2024-09-20"), 
+    Art, 
+    new Map<number, Visitor>
+    );
+
 const eventServ = new EventService();
 
 const visitorServ = new VisitorService(eventServ);
 
+visitorServ.addVisitor(john);
+visitorServ.addVisitor(jane);
+
+visitorServ.listVisitors();
+
 eventServ.addEvent(music);
 eventServ.addEvent(art);
+eventServ.addEvent(art2);
 
 eventServ.addVisitorToEvent(1, john);
 eventServ.addVisitorToEvent(2, john);
-
 eventServ.addVisitorToEvent(2, jane);
+
 console.log(eventServ.filter(Art));
 
+console.log("MűvészExpo vendégei:");
 art.listVisitors();
 
 visitorServ.removeVisitor(2);
+visitorServ.listVisitors;
 
+console.log("MűvészExpo vendégei:");
 art.listVisitors();
